c7ef230d6238fe28377208aa5fff0e43fdb1edf3
andreyphm <andrewkrokhin@gmail.com>
2026-07-14 21:05:31 +0300

    Add C support, generalize recursive search function, add language detection based on input file's extension

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
 import tree_sitter_python as tspython
+import tree_sitter_c as tsc
+import tree_sitter_java as tsjava
 from tree_sitter import Language, Parser
+from pathlib import Path
 
-def seek_func(root_node, func_list):
+language_info = []
+language_info.append({"suffix": ".py",   "func_def": "function_definition", "tree_sitter": tspython, "target": "name"})
+language_info.append({"suffix": ".c",    "func_def": "function_definition", "tree_sitter": tsc,      "target": "declarator"})
+language_info.append({"suffix": ".java", "func_def": "method_declaration",  "tree_sitter": tsjava,   "target": " "})
+
+def seek_func(root_node, func_list, func_def, target):
     for i in range(root_node.child_count):
 
-        if (root_node.children[i].type == "function_definition"):
-            func_name = root_node.children[i].child_by_field_name("name").text.decode("utf-8")
+        if (root_node.children[i].type == func_def):
+            func_name = seek_func_name(root_node.children[i], target)
             func_code = root_node.children[i].text.decode("utf-8")
 
-            func_list.append({
-                "name": func_name,
-                "code": func_code
-            })
+            func_list.append({"name": func_name, "code": func_code})
 
-        seek_func(root_node.children[i], func_list)
+        seek_func(root_node.children[i], func_list, func_def, target)
 
-py_language = Language(tspython.language())
-parser = Parser(py_language)
+def seek_func_name(node, target):
+    if node.type == "identifier":
+        return node.text.decode("utf-8")
+    return seek_func_name(node.child_by_field_name(target), target)
 
-with open("test_programs/test.py", "rb") as file:
+file_path = input()
+
+file_suffix = Path(file_path).suffix
+for language in language_info:
+    if file_suffix == language["suffix"]:
+        current_language = language
+
+with open(file_path, "rb") as file:
     data = file.read()
+
+language = Language(current_language["tree_sitter"].language())
+parser = Parser(language)
 
 tree = parser.parse(data)
 
 func_list = []
-seek_func(tree.root_node, func_list)
+seek_func(tree.root_node, func_list, current_language["func_def"], current_language["target"])
 
 print(func_list)
+
+# print(tree.root_node.type)
+# print(tree.root_node.children[0].type)
+# print(tree.root_node.children[1].type)
+# print(tree.root_node.children[2].type)
+# print(tree.root_node.children[2].children[0].type)
+# print(tree.root_node.children[2].children[1].type)
+# print(tree.root_node.children[2].children[1].children[0].type)
+# print(tree.root_node.children[2].children[1].children[0].text)
+# print(tree.root_node.children[2].children[1].children[1].type)
+# print(tree.root_node.children[2].children[2].type)
---
+++ test_programs/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+static int sub(int a, int b)
+{
+    return a - b;
+}
+
+int* create_array(int size)
+{
+    int* array = (int*) calloc(size, sizeof(int));
+    return array;
+}
+
+int factorial(int n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int sum = add(2, 3);
+    printf("%d\n", factorial(sum));
+}
