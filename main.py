1aa345b334a7a263a99702d1e10a9857b06f8280
andreyphm <andrewkrokhin@gmail.com>
2026-07-14 12:28:03 +0300

    Parsed a hardcoded Python function and examined its tree

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from func-extractor!")
+import tree_sitter_python as tspython
+from tree_sitter import Language, Parser
 
+py_language = Language(tspython.language())
+parser = Parser(py_language)
 
-if __name__ == "__main__":
-    main()
+tree = parser.parse(
+       bytes(
+       """
+        def add(a,b):
+            return a + b
+       """,
+       "utf8"
+       ))
+
+func_name = tree.root_node.children[0].children[1].text
+print(func_name)
+
+func_body = tree.root_node.children[0].text
+print(func_body)
+
+print(tree.root_node.type)
+print(tree.root_node.children[0].type)
+print(tree.root_node.children[0].children[0].type)
+print(tree.root_node.children[0].children[1].type)
+print(tree.root_node.children[0].children[2].type)
+print(tree.root_node.children[0].children[3].type)
+print(tree.root_node.children[0].children[4].type)
+print(tree.root_node.children[0].children[4].children[0].type)
