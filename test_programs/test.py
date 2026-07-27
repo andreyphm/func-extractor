bef7b7399311a476cb91e450e6c5460bcc1a4f5c
andreyphm <andrewkrokhin@gmail.com>
2026-07-27 10:59:47 +0300

    Revert "Fix review comments"

--- test_programs/test.py
+++ test_programs/test.py
@@ -1,17 +1,14 @@
 class calculator:
-    def add(a, b):
+    def add(a,b):
         return a + b
 
-    def sub(a, b):
+    def sub(a,b):
         return a - b
-
 
 def hello():
     print("hello")
 
-
 def return_func_42():
     def return_42():
         return 42
-
     return return_42()
